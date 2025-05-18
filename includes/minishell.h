e0f5d3a3ccae0fb08aa242c7097ff759167d5d3c
furizalex <Alexanderchin714@gmail.com>
2025-05-18 11:35:35 +0800

    Log: Added seperate headers & cleaned a few things

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,12 +6,22 @@
 /*   By: alechin <alechin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 11:16:24 by alechin           #+#    #+#             */
-/*   Updated: 2025/05/18 11:17:00 by alechin          ###   ########.fr       */
+/*   Updated: 2025/05/18 11:32:36 by alechin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include <sys/wait.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <termios.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft/libft.h"
+# include "libft/ft_printf/ft_printf.h"
 
 /*	todo!() -> Data Structures	*/
 
