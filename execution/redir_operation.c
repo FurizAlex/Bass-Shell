77a76b14bb7d86245b8d0d5d344e07ad06bd5f9b
furizalex <Alexanderchin714@gmail.com>
2025-06-19 17:52:23 +0800

    Log: Did more work on redirection

--- execution/redir_operation.c
+++ execution/redir_operation.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   redir_op.c                                         :+:      :+:    :+:   */
+/*   redir_operation.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alechin <alechin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 15:15:10 by alechin           #+#    #+#             */
-/*   Updated: 2025/06/18 15:18:19 by alechin          ###   ########.fr       */
+/*   Updated: 2025/06/19 15:23:19 by alechin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,58 @@
 #include "execution.h"
 #include "parsing.h"
 
-int	in(void)
+int	in(char *filename, int *fd, char **env)
 {
-	return (0);
+	int	infile;
+	
+	infile = open(filename, O_RDONLY);
+	if (!infile || infile == -1)
+		error2exit("🍥 Fishy Error: Could not get infile", 1);
+	dup2(infile, 0);
+	dup2(fd[1], 1);
+	close(fd[0]);
+	close(fd[1]);
+	/* execute the command */
 }
 
-int	out(void)
+int	out(char *filename, int *fd, char **env)
 {
-	return (0);
+	int	outfile;
+
+	outfile = open(filename, O_WRONLY, O_CREAT, O_TRUNC, 0777);
+	if (!outfile || !outfile == -1)
+		error2exit("🍥 Fishy Error: Could not get infile", 1);
+	dup2(fd[0], 0);
+	dup2(outfile, 1);
+	close(fd[0]);
+	close(fd[1]);
+	close(outfile);
+	/* execute the command */
 }
 
-int	append(void)
+int	append(char *filename, int *fd, char **env)
 {
-	return (0);
+	int	outfile;
+
+	outfile = open(filename, O_WRONLY, O_CREAT, O_APPEND, 0777);
+	if (!outfile || !outfile == -1)
+		error2exit("🍥 Fishy Error: Couldn't append file", 1);
+	dup2(fd[0], 0);
+	dup2(outfile, 1);
+	close(fd[0]);
+	close(fd[1]);
+	close(outfile);
+	/* execute the command */
 }
 
-int	heredoc(void)
+int	heredoc(char *filename, int *fd, char **env, char *limiter)
 {
-	return (0);
+	char *line;
+
+	if (pipe(fd) == -1)
+		error2exit("🍥 Fishy Error: Could not get infile", 1);
+	while (1)
+	{
+		
+	}
 }
---
+++ expansion/heredoc.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alechin <alechin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/19 15:25:49 by alechin           #+#    #+#             */
+/*   Updated: 2025/06/19 17:51:53 by alechin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include "execution.h"
+
+void	read_heredoc(char *line, int fd, char *delimiter)
+{
+	char	*doc;
+	int		i;
+
+	i = 0;
+	doc = ft_strdup("");
+	if (!line)
+		error2exit("🍥 Fishy Error: No line in heredoc\n", 1);
+	while (1)
+	{
+		line = readline("> ");
+		if (!line)
+			error2exit("🍥 Fishy Warning: Heredoc delimited by EOF\n", 2);
+		if (!line || !comp(line, delimiter))
+		{
+			free(line);
+			break ;
+		}
+		write(fd[1], line, ft_strlen(line));
+		dup2(fd[0], 0);
+		close(fd[0], 0);
+	}
+}
